a15cf63a740a804851f3723b9dc1de329e670a1c
Christopher Sexton <github@codeography.com>
2014-09-20 16:33:19 -0400

    Add wav support

--- music-player/lib/audio_player.rb
+++ music-player/lib/audio_player.rb
@@ -6,11 +6,32 @@ class AudioPlayer
   end
 
   def play
-    puts "playing #{file}"
+    case File.extname(file)
+    when ".mp3"
+      play_mp3
+    when ".wav"
+      play_wav
+    else
+      raise "Don't know how to play #{file}"
+    end
+  end
+
+  def play_wav
+    puts "playing wav #{file}"
+    if File.which('afplay')
+      `afplay "#{file}"`
+    else File.which('mpg321')
+      `aplay "#{file}"`
+    end
+  end
+
+  def play_mp3
+    puts "playing mp3 #{file}"
     if File.which('afplay')
       `afplay "#{file}"`
     else File.which('mpg321')
       `mpg321 "#{file}"`
     end
+
   end
 end
